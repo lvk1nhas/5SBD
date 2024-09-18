6d46ad75481da6de2829de7a5184023218fd711e
Lucas Melo <melolucasbpm@gmail.com>
2024-09-18 16:01:13 -0300

    Update bulk_insert_and_select_commands.sql

--- bulk_insert_and_select_commands.sql
+++ bulk_insert_and_select_commands.sql
@@ -1,4 +1,4 @@
-1 º comando: SQL pra importação dos dados em arquivo CSV:
+Comando: SQL pra importação dos dados em arquivo CSV:
 
 LOAD DATA INFILE 'C:/Users/casol/Downloads/produtos.csv'
 INTO TABLE wl_produtos
@@ -8,14 +8,14 @@ LINES TERMINATED BY '\n'
 (id, codigo, nome, valor, quantidade);
 
 
-
-2 º comando: Selecionar clientes sem repetição
-SELECT DISTINCT codigoComprador, email, endereco, CEP, UF, pais FROM wl_tempdata GROUP BY codigoComprador;
-
-3º comando: Inserindo dados do TXT no banco
+Comando: Inserindo dados do TXT no banco
 LOAD DATA INFILE 'C:/Users/casol/Downloads/pedidos.txt'
 INTO TABLE wl_tempdata
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 (codigoPedido, dataPedido, SKU, UPC, nomeProduto, quantidade, valor, frete, email, codigoComprador, nomeComprador, endereco, CEP, UF, pais);
+
+
+Comando: Selecionar clientes sem repetição
+SELECT DISTINCT codigoComprador, email, endereco, CEP, UF, pais FROM wl_tempdata GROUP BY codigoComprador;
