177d93196ed511cab49a31cd4662c4edf0a5e68a
Lucas Melo <melolucasbpm@gmail.com>
2024-10-09 20:21:26 -0300

    Update AV1.sql

--- AV1.sql
+++ AV1.sql
@@ -94,41 +94,6 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `wl_processar_pedidos_v2` ()   BEGIN
   
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET pronto = 1;
 
-       -- 1. Inserindo novos clientes que ainda não estão na base
-    INSERT INTO wl_clientes (codigoComprador, nomeComprador, email)
-    SELECT DISTINCT codigoComprador, nomeComprador, email
-    FROM wl_tempdata 
-    WHERE NOT EXISTS (SELECT 1 FROM wl_clientes WHERE codigoComprador = wl_tempdata.codigoComprador);
-
-    -- 2. Inserindo novos pedidos que ainda não existem na tabela de pedidos
-    INSERT INTO wl_pedidos (codigoPedido, codigoComprador, dataPedido, valor, status)
-    SELECT DISTINCT codigoPedido, codigoComprador, dataPedido, valor, 'pendente'
-    FROM wl_tempdata 
-    WHERE NOT EXISTS (SELECT 1 FROM wl_pedidos WHERE codigoPedido = wl_tempdata.codigoPedido);
-
-    -- 3. Inserindo novos produtos na tabela de produtos
-    INSERT INTO wl_produtos (SKU, UPC, nomeProduto, valor)
-    SELECT DISTINCT SKU, UPC, nomeProduto, ROUND(valor / quantidade, 2)
-    FROM wl_tempdata 
-    WHERE NOT EXISTS (SELECT 1 FROM wl_produtos WHERE SKU = wl_tempdata.SKU);
-
-    -- 4. Inserindo itens de pedido na tabela de itens de pedido
-    INSERT INTO wl_itens_pedidos (codigoPedido, SKU, quantidade, valor_unitario)
-    SELECT DISTINCT codigoPedido, SKU, quantidade, ROUND(valor / quantidade, 2)
-    FROM wl_tempdata 
-    WHERE NOT EXISTS (SELECT 1 FROM wl_itens_pedidos WHERE codigoPedido = wl_tempdata.codigoPedido AND SKU = wl_tempdata.SKU);
-
-    -- 5. Inserindo dados de entrega na tabela de entregas
-    INSERT INTO wl_entregas (codigoPedido, endereco, CEP, UF, pais, valor)
-    SELECT DISTINCT codigoPedido, endereco, CEP, UF, pais, frete
-    FROM wl_tempdata 
-    WHERE NOT EXISTS (SELECT 1 FROM wl_entregas WHERE codigoPedido = wl_tempdata.codigoPedido);
-
-    -- 6. Inserindo produtos na tabela de estoque, se não existirem
-    INSERT INTO wl_estoque (SKU)
-    SELECT DISTINCT SKU FROM wl_tempdata
-    WHERE NOT EXISTS (SELECT 1 FROM wl_estoque WHERE SKU = wl_tempdata.SKU);
-
 
     OPEN cursor_pedidos;
 
