08d770aaf362bdb82794f45e69f25519219af9b4
Lucas Melo <melolucasbpm@gmail.com>
2024-10-15 23:56:40 -0300

    Update AV1.sql

--- AV1.sql
+++ AV1.sql
@@ -69,18 +69,18 @@ DELIMITER $$
 --
 -- Procedimentos
 --
-CREATE DEFINER=root@localhost PROCEDURE ProcessarEstoque ()   BEGIN
+CREATE DEFINER=root@localhost PROCEDURE RecarregarEstoque ()   BEGIN
 
     -- Declaração de variáveis para armazenar os valores do cursor
     DECLARE v_SKU VARCHAR(20);  
     DECLARE v_quantidade INT;  
-    DECLARE terminou INT DEFAULT 0;  -- Alterado o nome de 'pronto' para 'terminou' para diferenciar
+    DECLARE terminou INT DEFAULT 0; 
 
     -- Definição do cursor para selecionar dados da tabela temporária
     DECLARE cursor_estoque CURSOR FOR SELECT SKU, quantidade FROM wl_tempdata_estoque;
 
     -- Manipulador para lidar com o fim do cursor
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET terminou = 1;  -- Alterei o nome do manipulador
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET terminou = 1; 
 
     -- Abrindo o cursor para processar as linhas da tabela temporária
     OPEN cursor_estoque;
@@ -91,7 +91,7 @@ CREATE DEFINER=root@localhost PROCEDURE ProcessarEstoque ()   BEGIN
         -- Buscando os valores da próxima linha do cursor e atribuindo às variáveis
         FETCH cursor_estoque INTO v_SKU, v_quantidade;
 
-        -- Se o cursor já leu todas as linhas, saímos do loop
+        -- Se o cursor já leu todas as linhas, saio
         IF terminou THEN
             LEAVE movimentacao_loop;  -- Sair do loop quando terminar
         END IF;
