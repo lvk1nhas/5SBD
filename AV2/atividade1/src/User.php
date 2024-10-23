64deac57238b72e9f2d4d7a38a63e6bb25405faf
Lucas Melo <melolucasbpm@gmail.com>
2024-10-23 20:45:57 -0300

    Update User.php

--- AV2/atividade1/src/User.php
+++ AV2/atividade1/src/User.php
@@ -6,4 +6,30 @@ class User extends Model {
     protected $table = 'users';
     public $timestamps = true; // Usar created_at e updated_at automaticamente
     protected $fillable = ['name', 'email', 'password']; // Campos que podem ser preenchidos em massa
+
+    public static function createUser($data) {
+        if (self::emailExists($data['email'])) {
+            throw new Exception("Email já cadastrado!");
+        }
+        return self::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => password_hash($data['password'], PASSWORD_DEFAULT)
+        ]);
+    }
+
+    public static function emailExists($email) {
+        return self::where('email', $email)->exists();
+    }
+
+    public function updateUser($data) {
+        $this->name = $data['name'];
+        $this->email = $data['email'];
+        $this->save();
+    }
+    
+    public function deleteUser() {
+        return $this->delete();
+    }
 }
+
